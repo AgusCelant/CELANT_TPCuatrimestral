f2a917d203c1480425e759c29ef8d75a2b2f8d0d
Agustin Celant <agustin_celant@live.com.ar>
2018-09-17 21:04:21 -0300

    cambios

--- Dominio/Paciente.cs
+++ Dominio/Paciente.cs
@@ -6,8 +6,13 @@ using System.Threading.Tasks;
 
 namespace Dominio
 {
-    public class Paciente : Persona
+    public class Paciente
     {
-
+        public int IdPaciente { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Edad { get; set; }
+        public int DNI { get; set; }
+        public DateTime Fnac{ get; set; }
     }
 }
